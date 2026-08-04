44f92695ea747b3779d1de0687f14822f9759cfd
Madiesse Sonia <soniariquielle@gmail.com>
2026-08-05 01:11:17 +0200

    Force HTTPS scheme in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 use Illuminate\Pagination\Paginator;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // 👈 1. Ajoutez cet import en haut
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,8 +21,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+
+        // 👈 2. Ajoutez cette ligne pour forcer le HTTPS hors du mode local
+        if (config('app.env') !== 'local') {
+            URL::forceScheme('https');
+        }   
         
-    Paginator::useBootstrapFive();
+        Paginator::useBootstrapFive();
 
     }
 }
